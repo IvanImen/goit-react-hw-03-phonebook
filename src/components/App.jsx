77feebd95270e83365dd338e6963a7f57c54ec3e
IvanImen <vanyagarret@gmail.com>
2024-01-04 03:21:53 +0200

    hw3-task1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,26 @@ import { nanoid } from 'nanoid';
 import React, { Component } from 'react';
 import { ContactForm, ContactsList, Filter } from 'components';
 
+const LOCAL_STORAGE_KEY = 'contacts';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (contacts) this.setState({ contacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
 
   handleFilterChange = e => {
     this.setState({ filter: e.target.value.trim().toLowerCase() });
